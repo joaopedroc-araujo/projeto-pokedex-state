d55aa17fa6393bad075a876d3d241e5ad00510ee
João <costaaraujojoaopedro@gmail.com>
2024-01-12 11:09:17 -0300

    FEAT: implementa filtros e estilização

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,18 +1,48 @@
+/* eslint-disable no-shadow */
 import React from 'react';
 import { arrayOf } from 'prop-types';
 
 import Pokemon from './Pokemon';
 import { pokemonType } from '../types';
 
+import './Pokedex.css';
+
 class Pokedex extends React.Component {
   state = {
     pokemonIndex: 0,
+    pokemonTypes: [],
+    pokemonType: 'All',
+    // eslint-disable-next-line react/destructuring-assignment
+    filteredPokemonList: this.props.pokemonList,
+  };
+
+  componentDidMount() {
+    this.catchPokemonTypes();
+  }
+
+  catchPokemonTypes = () => {
+    const { pokemonList } = this.props;
+    const allTypes = pokemonList.map((pokemon) => pokemon.type);
+    const uniqueTypes = [...new Set(allTypes)];
+    this.setState({ pokemonTypes: uniqueTypes });
+  };
+
+  handlePokemonFilter = async (type) => {
+    const { pokemonList } = this.props;
+    const filteredPokemonList = type === 'All' ? pokemonList
+      : pokemonList.filter((pokemon) => pokemon.type === type);
+
+    this.setState({
+      pokemonType: type,
+      pokemonIndex: 0,
+      filteredPokemonList,
+    });
   };
 
   handleNextPokemon = () => {
-    const { pokemonIndex } = this.state;
-    const { pokemonList } = this.props;
-    if (pokemonIndex === pokemonList.length - 1) {
+    const { pokemonIndex, filteredPokemonList } = this.state;
+
+    if (pokemonIndex === filteredPokemonList.length - 1) {
       this.setState({ pokemonIndex: 0 });
     } else {
       this.setState({ pokemonIndex: pokemonIndex + 1 });
@@ -20,17 +50,66 @@ class Pokedex extends React.Component {
   };
 
   render() {
-    const { pokemonList } = this.props;
-    const { pokemonIndex } = this.state;
-    const currentPokemon = pokemonList[pokemonIndex];
+    // const { pokemonList } = this.props;
+    // eslint-disable-next-line no-shadow
+    const {
+      pokemonIndex,
+      pokemonTypes,
+      pokemonType,
+      filteredPokemonList,
+    } = this.state;
+
+    const numberOfPokemons = filteredPokemonList.length;
+    const currentPokemon = filteredPokemonList[pokemonIndex];
 
     return (
       <>
         <h1> Pokédex </h1>
-        <div className="pokedex">
-          <Pokemon key={ currentPokemon.id } pokemon={ currentPokemon } />
+        <div className="type-buttons">
+          {pokemonTypes.map((type) => (
+            <button
+              key={ type }
+              type="button"
+              // disabled={ filteredPokemonList.length === 1 }
+              value={ type }
+              className="type-button"
+              onClick={ () => this.handlePokemonFilter(type) }
+            >
+              { type }
+            </button>
+          )) }
+          <button
+            className="type-button"
+            value="All"
+            onClick={ () => this.handlePokemonFilter('All') }
+          >
+            All
+
+          </button>
         </div>
-        <button type="button" onClick={ this.handleNextPokemon }>Próximo pokémon</button>
+        <span className="pokemon-count">
+          Total de Pokémons:
+          {' '}
+          {numberOfPokemons}
+          {' '}
+        </span>
+        <div className="pokedex">
+          {currentPokemon
+          && <Pokemon
+            key={ currentPokemon.id }
+            pokemon={ currentPokemon }
+            pokemonType={ pokemonType }
+          />}
+          {numberOfPokemons > 1 && (
+            <button
+              type="button"
+              onClick={ this.handleNextPokemon }
+              className="nextButton"
+            >
+              Próximo pokémon
+            </button>
+          )}
+        </div>
       </>
     );
   }
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -2,24 +2,30 @@ import React from 'react';
 
 import { pokemonType } from '../types';
 
+import './Pokemon.css';
+
 class Pokemon extends React.Component {
   render() {
     const { pokemon } = this.props;
     const { name, type, averageWeight, image } = pokemon;
 
     return (
-      <div className="pokemon">
-        <div>
-          <p>{name}</p>
-          <p>{type}</p>
-          <p>
-            Average weight:
-            <span>
-              {` ${averageWeight.value} ${averageWeight.measurementUnit}`}
-            </span>
-          </p>
-        </div>
-        <img src={ image } alt={ `${name}` } />
+      <div
+        className="pokemon"
+      >
+        <h3>{name}</h3>
+        <img
+          src={ image }
+          alt={ `${name}` }
+          className="pokemon-image"
+        />
+        <p>{type}</p>
+        <p>
+          Average weight:
+          <span>
+            {` ${averageWeight.value} ${averageWeight.measurementUnit}`}
+          </span>
+        </p>
       </div>
     );
   }
